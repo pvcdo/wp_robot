1fafe19c4900d4f4e8497690db9c659be4ae5c2c
Paulo Oliveira <paulocivictor@gmail.com>
2022-12-08 18:08:46 -0300

    trabalhando em estágios e entregando respostas na ordem correta

--- index.js
+++ index.js
@@ -1,29 +1,58 @@
 const wppconnect = require('@wppconnect-team/wppconnect');
 
+var userStages = []
+
 wppconnect.create({
   session: 'whatsbot',
   autoClose: false,
   puppeteerOptions: { args: ['--no-sandbox'] }
 })
-  .then((client) =>  
+  .then((client) =>
     client.onMessage((message) => {
-      console.log('Mensagem digitada pelo usuário: ' + message.body);
-      client.sendText(message.from, `Seja bem-vindo à PVC Digital!
-      
-      Digite: 
-      1 para tal
-      2 para pão duro
-      3 capitão
-
-      `)
-        .then((result) => {
-          console.log('Pong retornado: ', result); 
-        })
-        .catch((erro) => {
-          console.error('ERRO: ', erro);
-        });
-    })
-  )    
+      stages(client, message);
+    }))
   .catch((error) =>
-    console.log(error)
-  );
+    console.log(error));
+
+
+//  Stages = Olá  >>  Nome  >>  CPF  >> Fim
+async function stages(client, message) {
+  let stage = userStages[message.from];
+  switch (stage) {
+    case 'Nome':
+      const nome = message.body;
+      await sendWppMessage(client, message.from, 'Obrigada, ' + nome);
+
+      await sendWppMessage(client, message.from, 'Digite seu *CPF*:');
+      userStages[message.from] = 'CPF';
+      break;
+    case 'CPF':
+      const cpf = message.body;
+      await sendWppMessage(client, message.from, 'Obrigada por informar seu CPF: ' + cpf);
+      await sendWppMessage(client, message.from, 'Fim');
+      userStages[message.from] = 'Fim';
+      break;
+    case 'Fim':
+      await sendWppMessage(client, message.from, 'Fim');
+      break;
+    default: // Olá 
+      console.log('*Usuário atual* from:' + message.from);
+      await sendWppMessage(client, message.from, 'Bem vindo ao Robô de Whatsapp do AppBasicão!');
+      await sendWppMessage(client, message.from, 'Digite seu *NOME*:');
+      userStages[message.from] = 'Nome';
+  }
+}
+
+function sendWppMessage(client, sendTo, text) {
+  return new Promise((resolve,reject) => {
+    client
+    .sendText(sendTo, text)
+    .then((result) => {
+      resolve()
+    })
+    .catch((erro) => {
+      console.error('ERRO: ', erro);
+      reject()
+    });
+  }) 
+}
